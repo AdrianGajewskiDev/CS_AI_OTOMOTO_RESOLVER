36783fab89ddff6ef2c8e16ff274f782291be21c
Adrian Gajewski <adrian.gajewski001@gmail.com>
2024-09-06 23:45:09 +0200

    added basic lambda def

---
+++ build.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+rm -r build
+pip install poetry
+
+poetry install --no-dev
+mkdir build && cd build
+cp -r $(poetry env info -p)/lib/*/site-packages/* .
+cp -r ../otomoto_resolver .
+cp ../handler.py .
+zip -r ../otomoto-resolver.zip .
