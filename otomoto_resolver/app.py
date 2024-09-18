b88e6756bfd2122ba960daaae93db80a2017e54f
Adrian Gajewski <adrian.gajewski001@gmail.com>
2024-09-18 10:33:16 +0200

    added reading seed data from lambda event

--- otomoto_resolver/app.py
+++ otomoto_resolver/app.py
@@ -1,9 +1,9 @@
 from otomoto_resolver.scraper.start_up_scraper import startup_scraper
-from otomoto_resolver.seed_data_resolvers.dynamodb_streams_seed_data_resolver import DynamoDBStreamsSeedDataResolver
+from otomoto_resolver.seed_data_resolvers.lambda_event_seed_data_resolver import LambdaEventSeedDataResolver
 from otomoto_resolver.services.S3WriterService import S3WriterService
 
 
 def handler(event: dict, context):
-    seed_data_provider = DynamoDBStreamsSeedDataResolver(event)
+    seed_data_provider = LambdaEventSeedDataResolver(event)
     result_writer_service = S3WriterService()
     return startup_scraper(seed_data_provider, result_writer_service)
---
+++ otomoto_resolver/seed_data_resolvers/lambda_event_seed_data_resolver.py
@@ -0,0 +1,31 @@
+import json
+from otomoto_resolver.logging.logger import InternalLogger
+from otomoto_resolver.seed_data_resolvers.seed_data_resolver import SeedDataResolver
+
+
+class LambdaEventSeedDataResolver(SeedDataResolver):
+    def __init__(self, event: dict):
+        self.event = event
+
+    def get_seed_data(self) -> dict:
+        InternalLogger.LogInfo("Getting seed data from lambda event. {}".format(json.dumps(self.event)))        
+        seed_data = self.event["seed_data"]
+        task_id = self.event["task_id"]
+        created_date = self.event["created_date"]
+
+        return {
+            "seed_data": {
+                "Type": seed_data["Type"],
+                "Make": seed_data["Make"],
+                "Model": seed_data["Model"],
+                "ProductionYear": seed_data["ProductionYear"],
+                "FuelType": seed_data["FuelType"],
+                "Mileage": seed_data["Mileage"],
+                "EngineCapacity": seed_data["EngineCapacity"],
+                "Transmission": seed_data["Transmission"],
+                "EnginePower": seed_data["EnginePower"],
+                "Generation": seed_data["Generation"] if "Generation" in seed_data and seed_data else None,
+            },
+            "task_id": task_id,
+            "created_date": created_date
+        }
