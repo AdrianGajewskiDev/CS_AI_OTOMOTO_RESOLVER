e077e4c2cd2df9411c779f62ac2e6df566a95622
Adrian Gajewski <adrian.gajewski001@gmail.com>
2024-09-26 12:31:36 +0200

    test trigger build

--- otomoto_resolver/app.py
+++ otomoto_resolver/app.py
@@ -3,7 +3,6 @@ from otomoto_resolver.scraper.start_up_scraper import startup_scraper
 from otomoto_resolver.seed_data_resolvers.lambda_event_seed_data_resolver import LambdaEventSeedDataResolver
 from otomoto_resolver.services.S3WriterService import S3WriterService
 
-
 def handler(event: dict, context):
     seed_data_provider = LambdaEventSeedDataResolver(event)
     result_writer_service = S3WriterService()
